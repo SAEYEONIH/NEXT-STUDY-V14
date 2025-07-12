9e5936c94a2c9fbfa08085133b165def7dc44622
SAEYEONIH <syeoniih@gmail.com>
2025-07-12 14:22:14 +0900

    스타일링 및 레이아웃

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import SearchableLayout from "@/components/searchable-layout";
+import style from"./index.module.css";
+import { ReactNode } from "react";
+
 export default function Home() {
   return (
     <>
-    <h1>Next.js Study</h1>
+    <h1 className={style.h1} >Next.js Study</h1>
     </>
   );
+}
+
+Home.getLayout =(page: ReactNode) =>{
+  return <SearchableLayout>{page}</SearchableLayout>;
 }
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,14 +1,18 @@
+import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
+import { ReactNode } from "react";
 
 export default function Page() {
   const router = useRouter();
   const { q } = router.query;
 
   return (
-    <>
-      <div>
-        <h1>Search Page {q}</h1>
-      </div>
-    </>
+    <div>
+      <h1>Search Page {q}</h1>
+    </div>
   );
+}
+
+Page.getLayout =(page: ReactNode) =>{
+  return <SearchableLayout>{page}</SearchableLayout>;
 }
